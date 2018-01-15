c58da8bf9b6053440268f8d0b963c015cf88fb7e
Lauren <lmhavrin@gmail.com>
2018-01-15 11:58:46 -0600

    Exercise 2-7: Stripping Names

---
+++ chapter-two/stripping_names.py
@@ -0,0 +1,15 @@
+# Exercise 2-7: Stripping Names
+my_name = " Lauren "
+
+print(my_name)
+
+print(my_name.lstrip())
+
+print(my_name.rstrip())
+# wil not be able to see it
+
+print(my_name.strip())
+
+print("\t" + my_name)
+print("\n" + my_name)
+
