cc3e3e59177a23d1371c3f76748069c6bc42b1b0
Lauren <lmhavrin@gmail.com>
2018-01-15 11:46:01 -0600

    Exercise 2-6: Famous Quote 2

---
+++ chapter-two/famous_quote_two.py
@@ -0,0 +1,14 @@
+# what should have been done in Exercise 2-5 Famous Quote
+#print(""" Vince, Lombardi once said, "The difference between a successful person and others
+#is not a lack of strength, not a lack of knowledge, but rather a lack of will." """)
+
+# Exercise 2-6: Famous Quote
+famous_person = "Vince Lombardi"
+
+message = """ "The difference between a successful person and others is not
+a lack of strength, not a lack of knowledge, but rather a lack of will." """
+# should have put in message variable not quote according to exercise
+
+famous_quote = famous_person + " once said," + message
+
+print(famous_quote)
