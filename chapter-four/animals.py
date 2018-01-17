259b6a947871e053614b3b05520c168c2c4311e8
Lauren <lmhavrin@gmail.com>
2018-01-17 13:22:17 -0600

    Exercise 4-2: Animals

---
+++ chapter-four/animals.py
@@ -0,0 +1,7 @@
+# Exercise 4-2: Animals
+
+animals = ["dog", "cat", "rabbit"]
+
+for animal in animals:
+    print("A " + animal + " would make a great pet!")
+print("Any of these animals would make a great pet...")
