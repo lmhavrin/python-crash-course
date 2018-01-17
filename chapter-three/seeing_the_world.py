2df441db049658e88d54293ef172ee35c74ed07a
Lauren <lmhavrin@gmail.com>
2018-01-17 09:16:09 -0600

    Exercise 3-8: Seeing the World

---
+++ chapter-three/seeing_the_world.py
@@ -0,0 +1,22 @@
+# Exercise 3-8: Seeing the World
+
+favorite_locations = ["Netherlands", "New Zealand", "Colorado", "France", "England"]
+
+print(favorite_locations)
+print(sorted(favorite_locations))
+print(favorite_locations)
+
+print(sorted(favorite_locations, reverse=True))
+print(favorite_locations)
+
+favorite_locations.reverse()
+print(favorite_locations)
+
+favorite_locations.reverse()
+print(favorite_locations)
+
+favorite_locations.sort()
+print(favorite_locations)
+
+favorite_locations.sort(reverse=True)
+print(favorite_locations)
