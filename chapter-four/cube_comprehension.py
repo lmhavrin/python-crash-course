97b3ff66df4b25ffc1f608c6efbb6788bf2117e6
Lauren <lmhavrin@gmail.com>
2018-01-17 14:34:07 -0600

    Exercise 4-9: Cube Comprehension

---
+++ chapter-four/cube_comprehension.py
@@ -0,0 +1,6 @@
+# Exercise 4-9 Cube Comprehension
+cubes = [value**3 for value in range(1,11)]
+# list contains the for loop and starts with defining expression
+# of variables want stored in list
+print(cubes)
+
