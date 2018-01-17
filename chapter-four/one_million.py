ffe3dca6594928848fadead081c93f28bf8ec55c
Lauren <lmhavrin@gmail.com>
2018-01-17 13:44:36 -0600

    Exercise 4-4: One Million

---
+++ chapter-four/one_million.py
@@ -0,0 +1,5 @@
+# Exercise 4-4: One Million
+one_million = list(range(1, 1000001))
+# when you wrap list around a range function, output will be a list of numbers
+for number in one_million:
+    print(number)
