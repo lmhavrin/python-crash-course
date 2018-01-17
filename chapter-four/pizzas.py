c08681293573045f2062cd07d18bc2b2876db072
Lauren <lmhavrin@gmail.com>
2018-01-17 12:24:05 -0600

    Exercise 4-1: Pizzas

---
+++ chapter-four/pizzas.py
@@ -0,0 +1,10 @@
+# Exercise 4-1: Pizzas
+
+favorite_pizza = ["green peppers", "jalepeno", "mushroom"]
+for pizza in favorite_pizza:
+    print("I like " + pizza + " pizza!")
+
+print("I like pizza, pizza is good," +
+"\n" + favorite_pizza[0].title() + " pizza for a good day, " +
+"\n" + favorite_pizza[1].title() + " pizza for a spicy day, " +
+"\n" + favorite_pizza[-1].title() + " pizza for midday.")
