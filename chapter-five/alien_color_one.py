33a1ffee417847361432958c849a55e703a851e3
Lauren <lmhavrin@gmail.com>
2018-01-17 20:55:51 -0600

    Exercise 5-3: Alien Colors #1

---
+++ chapter-five/alien_color_one.py
@@ -0,0 +1,15 @@
+# Exercise 5-3: Alien Colors #1
+
+alien_color = "red"
+
+if alien_color == "green":
+    print("Player just earned 5 points")
+else:
+    print("No points for you!")
+
+alien_color = "green"
+
+if alien_color == "green":
+    print("Player just earned 5 points.")
+else:
+    print("No points for you!")
