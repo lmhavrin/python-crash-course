429f827f7c922127f2f961d3a400be5b116fb05f
Lauren <lmhavrin@gmail.com>
2018-01-18 08:07:51 -0600

    Exercise 5-11: Ordinal Numbers

---
+++ chapter-five/ordinal_numbers.py
@@ -0,0 +1,15 @@
+# Exercise 5-11: Ordinal Numbers
+
+ordinal_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for ordinal_number in ordinal_numbers:
+    if ordinal_number == 1:
+        print(str(ordinal_number) + "st")
+    elif ordinal_number == 2:
+        print(str(ordinal_number) + "nd")
+    elif ordinal_number == 3:
+        print(str(ordinal_number) + "rd")
+    else:
+        print(str(ordinal_number) + "th")
+# elif very important in this case, if did not use elif and just if
+# would repeat numbers
