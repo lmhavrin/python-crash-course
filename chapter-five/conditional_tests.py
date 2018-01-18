45eb22e2345647aaee673d97cff7e6b066ea3642
Lauren <lmhavrin@gmail.com>
2018-01-17 18:23:26 -0600

    Exercise 5-1: Conditional Tests

---
+++ chapter-five/conditional_tests.py
@@ -0,0 +1,40 @@
+# Exercise 5-1: Conditional Tests
+cat = "pregnant"
+
+print("Is cat == 'pregnant'? I predict true.")
+print(cat == "pregnant")
+
+print("\nIs cat == 'not pregnant'? I predict false.")
+print(cat == "not pregnant")
+
+color = "purple"
+
+print("\n Is color == 'purple', I predict true:")
+print(color == 'purple')
+
+print("\n Does color = 'blue', I predict false:")
+print(color == 'blue')
+
+print("\n Does color == 'brown', I predict false:")
+print(color == "brown")
+
+print("\n Does color == 'red', I predict false:")
+print(color == 'red' )
+
+print("\n Does color == 'taupe', I predict false:")
+print(color == 'taupe')
+
+tv_show = "south park"
+
+print("\n Does tv show == 'south park'? I predict true:")
+print(tv_show == "south park")
+
+mood = "happy"
+
+print("\n Does mood == happy? I predict true:")
+print(mood == "happy")
+
+month = "january"
+print("\n Does month == january? I predict true:")
+print(month == "january")
+
