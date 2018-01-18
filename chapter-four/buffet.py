e436791c495847043745f2477ad5d2f7ccdc9f62
Lauren <lmhavrin@gmail.com>
2018-01-17 18:05:15 -0600

    Exercise 4-13: Buffet

--- chapter-four/buffet.py
+++ chapter-four/buffet.py
@@ -17,3 +17,8 @@ for value in buffet:
 # you cannot modify a tuple, but can assign a new variable that holds a tuple,
 # and modify the list that way
 
+# style guide
+# four spaces per indent level
+# each line lesss than 80 characters
+
+# 4-14 and 4-15 code REVIEWED
