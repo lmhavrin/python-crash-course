09b3237eed27ff36cc58484aa44543a10a459022
Lauren <lmhavrin@gmail.com>
2018-01-18 08:01:01 -0600

    Exercise 5-9: No users

---
+++ chapter-five/no_users.py
@@ -0,0 +1,11 @@
+# Exercise 5-9: No Users
+
+current_users = []
+
+if current_users:
+        #remember above if var: and empty list will return
+        #false if empty and true if has one item in it
+    print("This is not an empty list")
+else:
+    print("We need to find some users!")
+    #correct syntax is key python is an unforgiving language
