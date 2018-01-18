1a35481dbd91c4f89d5ea065372e56924aa95879
Lauren <lmhavrin@gmail.com>
2018-01-17 21:20:35 -0600

    Exercise 5-7: Favorite Fruit

---
+++ chapter-five/favorite_fruit.py
@@ -0,0 +1,14 @@
+# Exercise 5-7: Favorite Fruit
+
+favorite_fruits = ["mangos", "greatfruit", "grapes"]
+
+if "mangos" in favorite_fruits:
+    print("You really like mangos!")
+if "greatfruit" in favorite_fruits:
+    print("You really like greatfruit!")
+if "grapes" in favorite_fruits:
+    print("You really like grapes!")
+if "apples" in favorite_fruits:
+    print("You really like apples.")
+if "kiwi" in favorite_fruits:
+    print("You really like kiwis.")
