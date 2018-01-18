c4c25b65ff47deddd78da28b42468ca930055b40
Lauren <lmhavrin@gmail.com>
2018-01-17 18:00:46 -0600

    Exercise 4-13: Buffet

---
+++ chapter-four/buffet.py
@@ -0,0 +1,19 @@
+# Exercise 4:13 Buffet
+
+buffet = ("chicken", "fish", "potatoes", "green beans", "apple pie")
+
+print("This restaurant buffet table offers: ")
+for item in buffet:
+    print("-" + item.title())
+
+# Modifying a tuple does not work
+#buffet[0] = "steak"
+
+buffet = ("steak", "fish", "potatoes", "asparagus", 'apple pie')
+print("\n This restaurant buffer table now serves: ")
+for value in buffet:
+    print("-" + value.title())
+
+# you cannot modify a tuple, but can assign a new variable that holds a tuple,
+# and modify the list that way
+
