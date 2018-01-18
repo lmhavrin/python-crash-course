36a062426dacf86f4b52d21db9e9e3220626b6e8
Lauren <lmhavrin@gmail.com>
2018-01-17 21:14:29 -0600

    Exercise 5-6: Stages of Life

---
+++ chapter-five/stages_of_life.py
@@ -0,0 +1,18 @@
+# Exercise 5-6 Stages of Life
+
+age = 26
+
+if age < 2:
+    print("This person is a baby.")
+elif age >2 and age < 4:
+    print("This person is a toddler.")
+elif age > 4 and age < 13:
+    print("This person is a kid.")
+elif age > 13 and age < 20:
+    print("This person is a teenager.")
+elif age > 20 and age < 65:
+    print("This person is an adult.")
+elif age > 65:
+    print("This person is an elder.")
+else:
+    print("Please enter a number for age.")
