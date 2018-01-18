3f1998c953dcce43adcb64e7f2b7dba680d1650f
Lauren <lmhavrin@gmail.com>
2018-01-17 21:03:47 -0600

    Exercise 5-4: Alien Colors 2

---
+++ chapter-five/alien_colors_two.py
@@ -0,0 +1,19 @@
+# Exercise 5-4: Alien Color #2
+
+alien_color = "purple"
+
+if alien_color == "green":
+    print("You just earned 5 points for shooting the alien.")
+elif alien_color != "green":
+    print("You earned 10 points for shooting an non-green alien.")
+else:
+    print("Please enter a color.")
+
+alien_color = "green"
+
+if alien_color == "green":
+    print("You just earned 5 points for shooting the alien.")
+elif alien_color != "green":
+    print("You earned 10 points for shooting a non-green alien.")
+else:
+    print("Please enter a color.")
