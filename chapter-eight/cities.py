10ebd4ffa0c3cc3c993aad544d25729d5c0dbd4d
Lauren <lmhavrin@gmail.com>
2018-02-03 17:43:26 -0600

    Exercise 8-5: Cities

---
+++ chapter-eight/cities.py
@@ -0,0 +1,9 @@
+# Exercise 8-5: Cities
+
+def describe_city(city, country = "United States"):
+    print(city.title() + " is in " + country.title() + "!")
+
+describe_city("chicago")
+describe_city("boston")
+describe_city("san francisco")
+describe_city("Trannsylvania", "Romania")
