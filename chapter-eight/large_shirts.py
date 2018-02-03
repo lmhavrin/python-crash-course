fc364a330cb8ec0f8816e9cc214db39602f47ced
Lauren <lmhavrin@gmail.com>
2018-02-03 17:35:01 -0600

    Exercise 8-4: Large Shirts

---
+++ chapter-eight/large_shirts.py
@@ -0,0 +1,8 @@
+# Exercise 8-4: Large Shirts
+
+def make_shirt(size = "large", message = "I love Python"):
+    print("The size of the shirt is " + size + " and the message is " + message + ".")
+
+make_shirt()
+make_shirt("medium")
+make_shirt("small", "I love pizza")
