baeb8ffe9068abdba01c594189bf715bd99407af
Lauren <lmhavrin@gmail.com>
2018-02-03 17:30:04 -0600

    Exercise T-Shirt

---
+++ chapter-eight/t-shirt.py
@@ -0,0 +1,7 @@
+# Exercise 8-3: T-Shirt
+
+def make_shirt(size, message):
+    print("The size of the shirt is " + size + " and the message is " + message + ".")
+
+make_shirt("medium", "'This is my stay at home shirt'")
+make_shirt(size = "small", message = "'This is my work shirt'")
