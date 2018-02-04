195f10eb91ef33c37789fa6582e51097bf1a511d
Lauren <lmhavrin@gmail.com>
2018-02-04 16:01:16 -0600

    Exercise 8-6: City Names

---
+++ chapter-eight/city_names.py
@@ -0,0 +1,14 @@
+# Exercise 8-6: City Names
+
+def city_country(city, country):
+    return city.title() + ", " + country.title()
+
+print(city_country("naples", "united states"))
+print(city_country("chicago", "united states"))
+print(city_country("lake bluff", "united states"))
+
+# return is where function ends
+# not every function needs return
+# function with return statement expects input and gives you output
+# so return is output statment
+
