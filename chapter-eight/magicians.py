d45dc19a946f2c3a9b4e7ff7a224221afb4b5bbf
Lauren <lmhavrin@gmail.com>
2018-02-04 18:36:36 -0600

    Exercise 8-9: Magicians

---
+++ chapter-eight/magicians.py
@@ -0,0 +1,8 @@
+# Exercise 8-9: Magicians
+magicians = ["bob", "larry", "joe", "bill"]
+
+def show_magicians(magicians):
+    for magician in magicians:
+        print("-" + magician.title())
+
+show_magicians(magicians)
