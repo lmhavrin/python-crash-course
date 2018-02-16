68d5e8344da3432ddfa672aa23c566d8fb8b8eed
Lauren <lmhavrin@gmail.com>
2018-02-16 17:06:07 -0600

    Exercise 9-10: Imported Restaurant

---
+++ chapter-nine/imported_restaurant.py
@@ -0,0 +1,8 @@
+# Exercise 9-10: Imported Restaurant
+
+# Using Exercise 9-1 Restaurant class to demonstrate
+from restaurant import Restaurant
+
+restaurant_one = Restaurant("Casa Bonita", "Mexican")
+
+restaurant_one.describe_restaurant()
